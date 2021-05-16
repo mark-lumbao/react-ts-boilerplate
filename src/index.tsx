0d000125c152299257e3fe77511dc78873666dde
Mark Anthony Lumbao <markanthonylumbao@gmail.com>
2021-05-17 02:22:42 +0800

    Initial commit

---
+++ src/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+render(
+  <div>React TS</div>,
+  document.getElementById('root'),
+);
---
+++ webpack.config.ts
@@ -0,0 +1,65 @@
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import { CleanWebpackPlugin } from 'clean-webpack-plugin';
+import { Configuration } from 'webpack';
+
+const babelOptions = ({
+  presets: [
+    '@babel/preset-env',
+    '@babel/preset-react',
+    '@babel/preset-typescript',
+  ],
+})
+
+const devServer = ({
+  contentBase: `${__dirname}/public`,
+  compress: true,
+  publicPath: '/dist/',
+  port: process.env.PORT || 1234,
+  writeToDisk: true,
+  historyApiFallback: true,
+  overlay: {
+    errors: true,
+    warnings: true,
+  },
+  open: true,
+});
+
+const config: Configuration & { devServer: typeof devServer } = ({
+  entry: `${__dirname}/src/index.tsx`,
+  output: {
+    path: `${__dirname}/public`,
+    filename: '[id][hash].js',
+    chunkFilename: '[chunkhash].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|js)x?/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: babelOptions,
+      },
+      {
+        test: /\.(sc|c)ss$/,
+        use: [
+          'css-loader',
+          'style-loader',
+        ],
+      }
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', ',jsx'],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'React TS',
+      template: `${__dirname}/src/template.html`,
+      minify: true,
+    }),
+    new CleanWebpackPlugin(),
+  ],
+  devServer,
+});
+
+export default config;
