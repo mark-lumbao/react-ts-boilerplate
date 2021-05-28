80b57a2d08867ea42ae1d865d67ce67253107815
Mark Anthony Lumbao <markanthonylumbao@gmail.com>
2021-05-29 06:11:44 +0800

    fix: Ignore non js imports with jest

--- jest.config.js
+++ jest.config.js
@@ -10,4 +10,10 @@ module.exports = {
     'node_modules',
     '<rootDir>/src',
   ],
+  testPathIgnorePatterns: [
+    '<rootDir>/public',
+  ],
+  moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': 'jest-transform-stub',
+  },
 };
